db56b9e16edd8c865a09960df6f47d10ad127246
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-01-11 15:35:30 +0100

    added tasks crud and postman test for them

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Task\TaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -26,40 +27,81 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TaskRequest $request)
     {
-        //
+        $task = Task::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'project_id' => $request->project_id,
+            'category_id' => $request->category_id,
+            'user_id' => $request->user_id,
+            'status_id' => $request->status_id,
+        ]);
+
+        return response()->json(data: [
+            'task' => $task,
+            'message' => "Successfully created task!",
+        ], status: 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Task $task)
+    public function show($id)
     {
-        //
+        $task = Task::findOrFail($id);
+
+        return response()->json(data: [
+            'task' => $task,
+            'message' => "Showing task",
+        ], status: 200);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Task $task)
+    public function edit($id)
     {
-        //
+        $task = Task::findOrFail($id);
+
+        return response()->json(data: [
+            "task" => $task,
+            'message' => "Editing Task",
+        ], status: 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Task $task)
+    public function update(TaskRequest $request, $id)
     {
-        //
+        $task = Task::findOrFail($id);
+
+        $task->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            "project_id" => $request->project_id,
+            'category_id' => $request->category_id,
+            'user_id' => $request->user_id,
+            'status_id' => $request->status_id,
+        ]);
+
+        return response()->json(data: [
+            'task' => $task,
+            'message' => "Task updated successfully",
+        ], status: 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Task $task)
+    public function destroy($id)
     {
-        //
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return response()->json(data: [
+            'message' => "Task succesfully deleted!",
+        ] , status: 200);
     }
 }
---
+++ backend/tests/Feature/TaskApiTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use App\Models\Project;
+use App\Models\Status;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+
+
+class TaskApiTest extends TestCase
+{   
+    use RefreshDatabase;
+
+    protected $user;
+    protected $status;
+    protected $category;
+    protected $project;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create([
+            'country' => "North Macedonia",
+            "gender" => "male",
+            "adress" => "Skopje",
+            "phone" => "38970000000",
+        ]);
+
+        $this->status = Status::factory()->create();
+        $this->project = Project::factory()->create();
+        $this->category = Category::factory()->create();
+        
+        Sanctum::actingAs($this->user);
+    }
+    
+    public function test_user_can_create_task() {
+        Sanctum::actingAs($this->user);
+
+        $response = $this->postJson("/api/task/store", [
+            'title' => "Test Task",
+            'description' => "Test Description",
+            'project_id' => $this->project->id,
+            'category_id' => $this->category->id,
+            'user_id' => $this->user->id,
+            'status_id' => $this->status->id,
+        ]);
+        
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('tasks', [
+            'title' => "Test Task",
+            'project_id' => $this->project->id,
+            'category_id' => $this->category->id,
+            'user_id' => $this->user->id,
+            'status_id' => $this->status->id,
+        ]);
+    }
+
+    public function test_user_can_view_task() {
+        $task = Task::factory()->create([
+            'project_id' => $this->project->id,
+            'category_id' => $this->category->id,
+            'user_id' => $this->user->id,
+            'status_id' => $this->status->id,
+        ]);
+
+        $response = $this->getJson("/api/task/show/{$task->id}");
+
+        $response->assertStatus(200)
+        ->assertJsonFragment([
+            'title' => $task->title,
+        ]);
+    }
+
+    public function test_user_can_update_task() {
+        $task = Task::factory()->create([
+            'project_id' => $this->project->id,
+            'category_id' => $this->category->id,
+            'user_id' => $this->user->id,
+            'status_id' => $this->status->id,
+        ]);
+
+        $response = $this->putJson("/api/task/update/{$task->id}", [
+            'title' => "Updated Task Title",
+            'description' => $task->description,
+            'project_id' => $this->project->id,
+            'category_id' => $this->category->id,
+            'user_id' => $this->user->id,
+            'status_id' => $this->status->id,
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('tasks', [
+            'id' => $task->id,
+            'title' => "Updated Task Title"
+        ]);
+    }
+
+    public function test_user_can_soft_delete_task() {
+        $task = Task::factory()->create([
+            'project_id' => $this->project->id,
+            'category_id' => $this->category->id,
+            'user_id' => $this->user->id,
+            'status_id' => $this->status->id,
+        ]);
+
+        $response = $this->deleteJson("/api/task/delete/{$task->id}");
+        $response->assertStatus(200);
+
+        $this->assertSoftDeleted('tasks', [
+            'id' => $task->id,
+        ]);
+    }
+}
