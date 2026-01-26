0b1adbd96743e62d681dfcf8022845ae83dfa1c9
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-01-26 22:37:22 +0100

    added logout functionality

--- frontend/src/components/navigations/MainNavigation.jsx
+++ frontend/src/components/navigations/MainNavigation.jsx
@@ -1,6 +1,18 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import AuthContext from "../../store/contexts/AuthContext";
+import { useContext } from "react";
 
 export default function MainNavigation() {
+    const {isAuthenticated, logout} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    async function handleLogout() {
+        await logout();
+        navigate("/login", { replace: true });
+    }
+
+    console.log("isAuthenticated:", isAuthenticated);
+
     return (
         <header className="p-3 bg-blue-950 text-gray-50 shadow-md shadow-amber-200">
             <div className="flex justify-between items- container mx-auto">
@@ -12,13 +24,19 @@ export default function MainNavigation() {
             <ul className="flex items-center gap-3"> 
                 <li>
                     <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="login">Login</Link>
-                    </li>
+                </li>               
                 <li>
                     <Link to="register">Register</Link>
                 </li>
+                {
+                    isAuthenticated ?  
+                     <li>
+                        <button onClick={handleLogout}>Logout</button>
+                    </li> :
+                    <li>
+                        <Link to="login">Login</Link>
+                    </li>                    
+                }
             </ul>
             </div>
             
